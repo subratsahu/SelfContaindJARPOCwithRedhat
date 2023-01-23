dcfb592c9b9f9c3200acf835d514f5e900571425
Subrat <subratsahu657@gmail.com>
2023-01-23 22:42:35 +0530

    initial commit

---
+++ POCWorkspace_RH_SpringBootJAR/containerisedbrms-model/src/main/java/in/libertygeneral/brms/model/Model.java
@@ -0,0 +1,7 @@
+package in.libertygeneral.brms.model;
+
+
+public class Model {
+
+
+}
---
+++ POCWorkspace_RH_SpringBootJAR/containerisedbrms-service/launch.bat
@@ -0,0 +1,54 @@
+@echo off
+
+set mavenInput="%*"
+
+if "%*" == "" (
+	echo No Maven arguments skipping maven build
+) else (
+	echo Running with user input: %mavenInput%
+	echo Running maven build on available project
+
+	call mvn -v >con
+
+	cd ..
+
+	for %%s in ("-model" "-kjar" "containerisedbrms-service") do (
+
+			cd *%%s
+			echo ===============================================================================
+            for %%I in (.) do echo %%~nxI
+            echo ===============================================================================
+
+			if exist "%M3_HOME%\bin\mvn.bat" (
+				call %M3_HOME%\bin\mvn.bat %* >con
+			) else (
+				call mvn %* >con
+			)
+
+			cd ..
+
+	)
+)
+
+goto :startapp
+
+:startapp
+	if not x%mavenInput:docker=%==x%mavenInput% (
+		echo Launching the application as docker container...
+		call docker run -d -p 8090:8090 --name containerisedbrms-service apps/containerisedbrms-service:1.0-SNAPSHOT
+	) else if not x%mavenInput:openshift=%==x%mavenInput% (
+		echo Launching the application on OpenShift...
+		call oc new-app containerisedbrms-service:1.0-SNAPSHOT
+		call oc expose svc/containerisedbrms-service
+	) else (
+		echo "Launching the application locally..."
+		setlocal EnableDelayedExpansion
+		cd containerisedbrms-service
+		cd target
+		for /f "delims=" %%x in ('dir /od /b *.jar') do set latestjar=%%x
+		cd ..
+		call java -jar target\!latestjar!
+	)
+
+
+:end
